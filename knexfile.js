06083eefb59226d37f0be0bccb82d67e0f4ce823
Jeremy Roelfs <jeremyroelfs@icloud.com>
2016-03-30 16:06:32 -0700

    updated prod

--- knexfile.js
+++ knexfile.js
@@ -12,14 +12,5 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: process.env.DATABASE_URL + '?ssl=true'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'lexeme_knex_migrations'
-    }
   }
-
 };
